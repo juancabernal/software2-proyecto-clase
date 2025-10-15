ed2fef611e5a39e4e659a12ae2f3f62a7b4a506d
juancabernal <juancamilobernal2222@gmail.com>
2025-10-15 15:12:56 -0500

    Commit de Prueba Número 3

--- src/main/java/co/edu/uco/ucochallenge/UcoChallengeApplication.java
+++ src/main/java/co/edu/uco/ucochallenge/UcoChallengeApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 //Esta es la clase principal que arranca la aplicacion
+// La anotacion @SpringBootApplication indica que es una aplicacion Spring Boot
+// El metodo main es el punto de entrada de la aplicacion
+// SpringApplication.run() arranca la aplicacion
 @SpringBootApplication
 public class UcoChallengeApplication {
 
